2285572dbe648d1a51a3b214390e3241de101682
Clark Winkelmann <clark.winkelmann@gmail.com>
2015-04-14 15:32:36 +0200

    Ajout d'un README pour Linux et amélioration du script

--- scripts/compile-linux-64.sh
+++ scripts/compile-linux-64.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-# Script de compilation et empaquetage pour linux
+# Script de compilation et empaquetage pour linux (64bit)
 # Auteur: Clark Winkelmann
 # Date: avril 2015
 
 # Éléments répétés
 
 resdir='../res'
+docdir='../doc'
 profile='../src/Zombu.pro'
 exefile='Zombu'
-compiledir='linux-compile'
-deploydir='linux-deploy'
+compiledir='linux-compile-64'
+deploydir='linux-deploy-64'
+
+# Étapes préliminaires
+
+echo 'Génération des ressources...'
+bash generate-res.sh
+
+echo 'Génération des locales...'
+bash locales-convert.sh
 
 # Suppression et recréation des dossiers
 
+echo 'Nettoyage des répertoires...'
 rm -rf $compiledir
 rm -rf $deploydir
 
@@ -23,13 +33,17 @@ mkdir $deploydir
 
 cd $compiledir
 
-qmake ../"$profile" -spec linux-g++ "CONFIG+=release" "DEFINES+=DEPLOY"
+echo 'Exécution de QMake...'
+qmake ../"$profile" -spec linux-g++-64 "CONFIG+=release" "DEFINES+=DEPLOY"
 
+echo 'Excution de Make...'
 make
 
 cd ..
 
 # Copie des fichiers à déployer
+
+echo 'Copie des fichiers à déployer...'
 
 mkdir "$deploydir"/res/
 mkdir "$deploydir"/res/images/
@@ -37,18 +51,25 @@ mkdir "$deploydir"/res/locales/
 mkdir "$deploydir"/res/words/
 
 cp "$compiledir"/Zombu "$deploydir"/
+cp "$docdir"/README_Linux_64.md "$deploydir"/README.md
 cp "$resdir"/images/*.png "$deploydir"/res/images/
 cp "$resdir"/locales/*.qm "$deploydir"/res/locales/
 cp "$resdir"/words/*.txt "$deploydir"/res/words/
 
 # Création de l'archive
 
+echo 'Création de l''archive...'
 cd $deploydir
-tar czvf ../Zombu_Linux.tar.gz *
+tar czvf ../Zombu_Linux_64bit.tar.gz *
 cd ..
 
 # Nettoyage
 
+echo 'Nettoyage...'
 rm -rf $compiledir
 rm -rf $deploydir
+
+# Fin de l'exécution du script
+
+echo 'Terminé.'
 
